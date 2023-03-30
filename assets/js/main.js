6459bc9dee9b35de05c199b1967ad2b6b7dcc9a4
bryaanls <bryanluccas@hotmail.com>
2023-03-30 00:13:45 -0300

    deploy

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+function animateScroll() {
+    const dataAnime = document.querySelectorAll('[data-anime]');
+    const windowTop = window.innerHeight * 0.5;
+
+    dataAnime.forEach((e) => {
+        if (e.getBoundingClientRect().top - windowTop < 120) {
+            e.classList.add('animate')
+        } else {
+            e.classList.remove('animate')
+        }
+    })
+}
+
+window.addEventListener('scroll', () => {
+    animateScroll()
+})
