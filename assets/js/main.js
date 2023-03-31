3aad881174ac92b3522743f8cab9cc3b68ade68a
bryaanls <bryanluccas@hotmail.com>
2023-03-31 18:47:41 -0300

    deploy

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,15 +15,30 @@ window.addEventListener('scroll', () => {
     animateScroll()
 })
 
-
+const titleSanfona = document.querySelectorAll('.titleSeeMore');
 const seeMore = document.querySelectorAll('.seeMore');
 const sanfonaContent = document.querySelectorAll('.sanfona-content');
 const sanfona = document.querySelectorAll('.sanfona-card');
 
-seeMore.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        sanfonaContent[index].classList.toggle('active');
-        btn.classList.toggle('rotateBtn');
-        sanfona[index].classList.toggle('active2');
+function percorre(array) {
+    array.forEach((el, index) => {
+        el.addEventListener('click', () => {
+            toggleSanfona(el, index)
+        })
     })
-})
+}
+
+function toggleSanfona(el, index) {
+
+    if (el.classList.contains('seeMore')) {
+        el.classList.toggle('rotateBtn');
+    } else if (el.classList.contains('titleSeeMore')) {
+        el.classList.remove('rotateBtn')
+        seeMore[index].classList.toggle('rotateBtn');
+    }
+    sanfonaContent[index].classList.toggle('active');
+    sanfona[index].classList.toggle('active2');
+}
+
+percorre(titleSanfona)
+percorre(seeMore)
